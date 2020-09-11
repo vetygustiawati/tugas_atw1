6e5dfe9b69c608b622f50bd75060643d97df337d
vetygustiawati <71057826+vetygustiawati@users.noreply.github.com>
2020-09-12 06:02:55 +0700

    penambahan class anjing

--- index.php
+++ index.php
@@ -20,3 +20,21 @@ echo "Jumlah Kaki Sebanyak : $kucing->jumlah_kaki <br>";
 echo "Apakah Bisa Terbang ? $kucing->bisa_terbang <br>";
 echo "Suaranya : $kucing->suara <br>";
 echo "<hr>";
+
+class Anjing {
+	public $nama, $jumlah_kaki, $bisa_terbang, $suara;
+}
+
+$anjing = new Anjing;
+$anjing->nama = "Zeky";
+$anjing->jumlah_kaki = "4";
+$anjing->bisa_terbang = "Tidak bisa";
+$anjing->suara = "Gukguk";
+
+
+echo "Anjing  <br>";
+echo "Yang Bernama : $anjing->nama <br>";
+echo "Jumlah Kaki Sebanyak : $anjing->jumlah_kaki <br>";
+echo "Apakah Bisa Terbang ? $anjing->bisa_terbang <br>";
+echo "Suaranya : $anjing->suara <br>";
+echo "<hr>";
